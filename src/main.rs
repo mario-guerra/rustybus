51f14626fb0b00b13d5450f1ca35cd31970f9ea0
mario-guerra <marioguerra@microsoft.com>
2022-08-24 14:50:28 -0500

    Add project files

---
+++ src/main.rs
@@ -0,0 +1,174 @@
+use azure_messaging_servicebus::prelude::*;
+use rustdotenv::load;
+use serde_json::{Value};
+use async_std::task;
+use std::time::Duration;
+use std::env;
+use std::process;
+use std::io;
+use std::sync::mpsc;
+use std::sync::mpsc::{Sender};
+use std::sync::mpsc::TryRecvError;
+use std::{thread, time};
+
+
+struct Config {
+    action: String,
+    queue: String,
+}
+
+/// Handle parsing of command line arguments
+impl Config {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
+      if args.len() < 3 {
+          return Err("Missing arguments. Usage: rustybus <send/receive/peek> <queue name>");
+      }
+      let action = args[1].clone();
+      let queue = args[2].clone();
+  
+      Ok(Config { action, queue })
+    }
+}
+
+/// Handle reading of input streamed via pipe
+fn spawn_stdin_channel(tx: Sender<String>) {
+    thread::spawn(move || loop {
+        let mut buffer = String::new();
+        match io::stdin().read_line(&mut buffer) {
+            Ok(_n) => {
+                let x = tx.send(buffer);
+                if x.is_ok() {
+                    continue;
+                } else {
+                    break;
+                }
+            }                
+            Err(error) => {println!("Error reading from stdin. Error: {}", error); break;}
+        }
+    });
+}
+
+fn sleep(millis: u64) {
+    let duration = time::Duration::from_millis(millis);
+    thread::sleep(duration);
+}
+
+#[tokio::main]
+async fn main() {
+    // Spawn a thread + channel to check for piped input - valid for use with send command only
+    let (tx, rx) = mpsc::channel::<String>();
+    spawn_stdin_channel(tx);
+    sleep(1000);
+
+    // Check for piped input on receiver side of channel
+    let mut message_to_send: String = "".to_string();
+    loop{
+        match rx.try_recv() {
+            Ok(key) => { message_to_send.push_str(&key);},
+            Err(TryRecvError::Empty) => { 
+                if message_to_send.eq("") {
+                    message_to_send.push_str("Empty"); 
+                }
+                break;
+            },
+            Err(TryRecvError::Disconnected) => { 
+                if message_to_send.eq("") {
+                    message_to_send.push_str("Empty"); 
+                }
+                break;
+            },
+        };
+    };   
+     
+    // Figure out what I'm supposed to do based on command line args
+    let args: Vec<String> = env::args().collect();
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+    
+    // Load .env file
+    load(None);
+
+    // Load service bus client info from variables specified in .env file 
+    let service_bus_namespace = std::env::var("AZURE_SERVICE_BUS_NAMESPACE")
+      .expect("Service Bus Namespace not found in .env file");
+    let policy_name = std::env::var("AZURE_POLICY_NAME")
+      .expect("Policy name not found in .env file");
+    let policy_key = std::env::var("AZURE_POLICY_KEY")
+      .expect("Policy key not found in .env file");
+    let http_client = azure_core::new_http_client();
+
+    // Build service bus client
+    let client = Client::new(
+        http_client,
+        service_bus_namespace,
+        config.queue,
+        policy_name,
+        policy_key,
+    )
+    .expect("Failed to create client");
+
+    // Call helper functions to handle send/receive/peek
+    match config.action.as_str() {
+        "send" => {send(client, message_to_send).await},
+        "receive" => {receive(client).await},
+        "peek" => {peek(client).await},
+        _other => {println!("Invalid command")},
+    }
+}
+
+/// Handle sending messages
+async fn send(client: Client, message_to_send: String) {
+    // Remove newline chars from JSON string
+    let message = message_to_send.replace("\n", "");
+
+    task::block_on(async {
+        client.send_message(&message)
+            .await
+            .expect("Failed to send message");
+            // If the previous statement doesn't error out 
+            // we can assume the send was successful. 
+            println!("Message sent!");
+    });
+}
+
+/// Handle receiving messages. 
+async fn receive(client: Client) {
+        task::block_on(async {
+            let received_message = client
+            .receive_and_delete_message()
+            .await
+            .expect("Failed to receive message");
+            
+            let value: Result<Value, serde_json::Error> = serde_json::from_str(&received_message);
+
+            match value {
+                Ok(message) => {println!("Message: {}", message);},
+                Err(error) => {println!("Error with message: {}", error);},
+            }
+        });
+    }
+
+/// Handle peeking at messages
+async fn peek(client: Client) {
+    task::block_on(async {
+        let three_seconds = Duration::new(3,0);
+        let peek_message = client
+        .peek_lock_message2(Some(three_seconds))
+        .await
+        .expect("Failed to receive message");
+
+        let value: Result<Value, serde_json::Error> = serde_json::from_str(&peek_message.body());
+        match value {
+            Ok(message) => {
+                println!("Peek message: {}", message);
+                let unlock = peek_message.unlock_message().await;
+                if unlock.is_err() {
+                    println!("Unable to unlock message");
+                }
+            },
+            Err(_error) => {println!("No messages in queue");},
+        }
+    });
+}    
